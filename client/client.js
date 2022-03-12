28d0fb2dd0fe6240cad73e89f605e4466b92e6f2
Qureshi Owais <qureshiowais91@gmail.com>
2022-03-12 12:18:07 +0530

    call from client side

--- client/client.js
+++ client/client.js
@@ -1,16 +1,28 @@
 const login = "http://127.0.0.1:3000/api/auth/login";
 
-const options = {
-    method: 'POST',
-    body: JSON.stringify({
-        username: "Owais91",
-        password: "userTwo"
-    }),
-    headers: {
-        'Content-Type': 'application/json'
-    }
-}
+const username = document.querySelector('#username');
+const password = document.querySelector('#password');
 
-fetch(login,options)
-    .then(res => res.json())
-    .then(res => console.log(res.token));
+const Myform = document.querySelector('.myform');
+
+Myform.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(username.value);
+    console.log(password.value);
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify({
+            username: username.value,
+            password: password.value
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    fetch(login, options)
+        .then(res => res.json())
+        .then(res => document.write(res.token));
+})
+
