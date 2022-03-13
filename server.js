bd9208ca65f14ea7dd0e9ca39324a45f817e0248
Qureshi Owais <qureshiowais91@gmail.com>
2022-03-13 11:18:26 +0530

    get account

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
 const express = require("express");
 const register = require("./API/router/auth");
-const connectDB = require("./API/config/connectDB");
+const account = require("./API/router/doctor/account");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+// const connectDB = require("./API/config/connectDB");
 const cors = require("cors");
 
-let port = process.env.PORT || 4040;
 
-dotenv.config({ path: '/home/owais/Project/Wellness_v1/API/config/.env' })
-connectDB(process.env.MONGODB_URI);
 
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "*");
+dotenv.config({ path: '/home/owais/Project/Wellness_v1/API/config/.env' });
 
-//     if (req.method === 'OPTIONS') {
-//         res.header('Access-Control-Allow-Methods', '*');
-//         return res.status(200).json({});
-//     }
 
-//     next();
-// });
+async function connectDB(URI) {
+    try {
+       await mongoose.connect(URI).then((res) => {
+            console.log(`connected`);
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+connectDB("mongodb+srv://owais91:1kOyxuwdWM0gs4TX@cluster0.btnyu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+
 
 const app = express();
 
@@ -32,8 +36,8 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/auth", register);
-// app.use("api/appointment", appointment);
+app.use("/api/doctor", account);
 
-app.listen(port, () => {
-    console.log("Server Connected");
+app.listen(3000, () => {
+    console.log(`Server Connected 3000`);
 });
