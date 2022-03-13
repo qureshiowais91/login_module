e4e20997c4462e91296b1766ecb2009f8596c577
Qureshi Owais <qureshiowais91@gmail.com>
2022-03-13 14:50:07 +0530

    all accounts fix

--- API/controller/doctor.js
+++ API/controller/doctor.js
@@ -51,7 +51,7 @@ exports.removeaccount = async (req, res, next) => {
 
 exports.allaccounts = async (req, res, next) => {
 
-    const result = await user.find();
+    const result = await user.find(req.body);
 
     res.status(200).json({
         success: true,
