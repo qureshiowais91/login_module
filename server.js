cb1ba3f71f8afba44f4551196a3cce6adaaad84f
qureshi owais <25130147+qureshiowais91@users.noreply.github.com>
2022-03-16 00:07:51 +0530

    Update cors

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 const allowedOrigins = ['http://127.0.0.1:3000', 'http://localhost:5500', 'http://127.0.0.1:3500', 'http://localhost:4500'];
 
 var corsOptions = {
-    origin: 'http://localhost:5500',
+    origin: 'http://localhost:5500','http://127.0.0.1:5500',
     credentials: true
 };
 
