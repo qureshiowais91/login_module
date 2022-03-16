59ef5d7fee0d78d52ee431363f67d5b7af8f1565
Qureshi Owais <qureshiowais91@gmail.com>
2022-03-16 22:28:32 +0530

    new point

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const authentication = require("./API/router/auth");
-const doctor = require("./API/router/doctor");
+//import DB Connection
 const connectDB = require("./API/config/connectDB");
 const errorHandler = require("./API/middleware/error");
-// const connectDB = require("./API/config/connectDB");
-const cors = require("cors");
 
+//improt Router
+
+const user = require("./API/router/user");
+const doctor = require("./API/router/doctor");
+const authentication = require("./API/router/auth");
+
+// Cors and Assing PORT
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+
+//Dotenv Config Path 
 dotenv.config({ path: "/home/owais/Project/login_0_1/login_module/API/config/.env" });
 
+//Connect Database 
 connectDB(process.env.MONGODB_URI);
 
 const app = express();
-// const allowedOrigins = ['http://127.0.0.1:3000', 'http://localhost:5500', 'http://127.0.0.1:3500', 'http://localhost:4500'];
-
-// var corsOptions = {
-//     origin: 'http://127.0.0.1:5500',
-//     credentials: true
-// };
-
-// app.use(cors(corsOptions));
 
 // specify the allowed domains and set corsOptions to check them
 var whitelist = ['http://localhost:5500', 'http://127.0.0.1:5500'];
@@ -30,14 +31,15 @@ var corsOptions = {
     }
 };
 
+//Middleware
 app.use(express.json());
-
+// Router
 app.use("/api/auth", cors(corsOptions), authentication);
-app.use("/api/doctor", cors(corsOptions), doctor);
+app.use("/api/user", cors(corsOptions), user);
+app.use("/api/docter", cors(corsOptions), doctor);
 
+//  Middleware
 app.use(errorHandler);
-const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
     console.log(`Server Connected ${port}`);
 });
